2dca5a70dfdd998d341ba6b648ae301037cc0453
Henimbola ANDRIANATOANDRO <henimbola@gmail.com>
2022-06-10 23:55:01 +0300

    initial commit

---
+++ src/test/java/com/whispro/TodoList/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whispro.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
